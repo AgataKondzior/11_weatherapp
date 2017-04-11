8b4626b305401567069a899ab006762a589451a7
AgataKondzior <ak.kondzior@gmail.com>
2017-04-11 10:07:52 +0200

    Update weather.js

--- js/weather.js
+++ js/weather.js
@@ -21,13 +21,13 @@ $(document).ready(function() {
             success: function(weather) {
                 city = weather.city;
                 temp = weather.temp + "&deg;";
-                wcode = '<img class="climate_bg" src="images/weathericons/'+ weather.code + '.svg">';
+                wcode = '<img class="weathericon" src="images/weathericons/'+ weather.code + '.svg">';
                 wind = '<p>' + weather.wind.speed + '</p><p>' + weather.units.speed + '</p>';
                 humidity = weather.humidity + "%";
 
                 $(".location").text(city);
                 $(".temperature").html(temp);	
-                $(".climate.bg").html(wcode);
+                $(".climate_bg").html(wcode);
                 $(".windspeed").html(wind);
                 $(".humidity").text(humidity);
             },
